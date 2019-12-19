4b6950be1a6c99ba67845dbf8d6ae83e0cb3b72d
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-12-19 01:01:37 +0100

    fixed update function for nodes

--- js/utils/d3Functions.js
+++ js/utils/d3Functions.js
@@ -44,7 +44,6 @@ export async function createFramework() {
 	restart();
 
 	function restart() {
-		console.log(nodes)
 		if (links.length === 0) {
 			links.push(
 				{source: "startPoint", target: "Oceanen"},
@@ -84,14 +83,15 @@ export async function createFramework() {
 					return changeColorOnQtyCircle(d.qty)
 				}
 			})
+			.call(d3.drag()
+				.on("start", dragstarted)
+				.on("drag", dragged)
+				.on("end", dragended))
 			.on('click', function(d) {
 				if ( d.geoName === 'startPoint' ) { return }
-				else if( d.clicked === true ) {
-					removeLinks(d);
-					d.clicked = false
-				}
 				else {
-					addChildrenNodes(d) }
+					update(d)
+				}
 			})
 			.on("mouseover", function(d) {
 				tooltip
@@ -119,29 +119,95 @@ export async function createFramework() {
 		simulation.nodes(nodes)
 		simulation.force("link", d3.forceLink(links).id(function(d) { return d.geoName }).distance(100).strength(1));
 		simulation.force("charge", d3.forceManyBody().strength(-30))
-			.force("center", d3.forceCenter(width / 2, height / 2))
+		//	.force("center", d3.forceCenter(width / 2, height / 2))
 			.alphaTarget(1)
 			.on("tick", ticked);
 
 	}
 
-	function removeLinks(x) {
-		let result = [];
-		x.children.map(item => { result.push(item.geoName) });
+	function update(x) {
+		console.log(x)
+		const oldNodes = nodes;
+		const oldLinks = links;
 
-		//node filter part is buggy
-		node
-			.data(nodes.filter(function (eachNode) {
-				return eachNode !== function() {result.forEach(item => {return item})}
-				}))
+		if (x.clicked === false) {
+			addChildrenNodes(x)
+		}
+		else if (x.clicked === true) {
+			x.clicked = false;
 
-			.exit().remove();
+			let nodesToFilter = [];
+			x.children.map(child => {
+				nodesToFilter.push(child.geoName)
+			});
 
-		link
-			.data(links.filter(function(linkies) {
-				return linkies.source.geoName !== x.geoName}))
-			.exit().remove();
-		restart()
+			console.log(nodesToFilter)
+
+			nodes.forEach(function (d, i) {
+				x.children.map(child => {
+					if(d.geoName === child.geoName) {
+						nodes.splice(i, x.children.length)
+					}
+				})
+			})
+
+			node = node.data(nodes)
+			node.exit().remove();
+			node = node.enter()
+				.append('circle')
+				.attr('r', function(d) {
+					if (d.geoName === 'startPoint') { return 5 }
+					else if (d.qty <= 1000 ) { return 5 }
+					else if (d.qty > 1000 && d.qty <= 50000) { return 10 }
+					else if (d.qty > 50000 && d.qty <= 100000) { return 20 }
+					else if (d.qty > 100000 && d.qty <= 300000) { return 30 }
+					else if (d.qty >= 300000) { return 40 }
+
+				})
+				.attr('fill', function (d) {
+					if (d.geoName === 'startPoint') {
+						return 'orange'
+					} else {
+						return changeColorOnQtyCircle(d.qty)
+					}
+				})
+				.on('click', function(d) {
+					if ( d.geoName === 'startPoint' ) { return }
+					else {
+						update(d)
+					}
+				})
+				.on("mouseover", function(d) {
+					tooltip
+						.transition()
+						.duration(200)
+						.style("opacity", .9);
+					tooltip
+						.html(d.geoName + "<br/>"  + d.qty)
+						.style("left", (d3.event.pageX) + "px")
+						.style("top", (d3.event.pageY - 30) + "px");
+				})
+				.on("mouseout", function(d) {
+					tooltip.transition()
+						.duration(500)
+						.style("opacity", 0);
+				})
+				.merge(node);
+
+
+			link = link.data(links);
+			link.exit().remove();
+			link = link.enter().append("line").merge(link);
+// merging local variable link with array, with the global variable link array, so creating 1 array.
+
+			simulation.nodes(nodes)
+			simulation.force("link", d3.forceLink(links).id(function(d) { return d.geoName }).distance(100).strength(1));
+			simulation.force("charge", d3.forceManyBody().strength(-30))
+				//.force("center", d3.forceCenter(width / 2, height / 2))
+				.alphaTarget(1)
+				.on("tick", ticked);
+
+		}
 	}
 
 
@@ -174,6 +240,24 @@ export async function createFramework() {
 		node
 			.attr("cx", function(d) { return d.x })
 			.attr("cy", function(d) { return d.y })
+	}
+
+	//dragged function from https://blockbuilder.org/almsuarez/851ed69e6b5b9cacaa4f5b1295a36ebe
+	function dragstarted(d) {
+		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged(d) {
+		d.fx = d3.event.x;
+		d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+		if (!d3.event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
 	}
 
 	//reset function
